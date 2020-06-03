99033e48e46593901442e56e698480d9019496ec
teapuddles <kingsley.gleeson@gmail.com>
2020-06-03 15:20:45 -0400

    sqlite to pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_03_23_191458) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "keys", force: :cascade do |t|
     t.string "pitch"
     t.datetime "created_at", precision: 6, null: false
